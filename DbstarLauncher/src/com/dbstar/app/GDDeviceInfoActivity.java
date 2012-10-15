1648db26f2d59896651fc22c89fd7c9dd8ce228b
Luo Shidong <luosd09@126.com>
2012-10-15 16:17:25 +0800

    Launcher: settings, popups

---
+++ DbstarLauncher/src/com/dbstar/app/GDDeviceInfoActivity.java
@@ -0,0 +1,145 @@
+package com.dbstar.app;
+
+import com.dbstar.R;
+import com.dbstar.model.GDSettings;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class GDDeviceInfoActivity extends GDSettingActivity {
+	private static final String TAG = "GDDeviceInfoActivity";
+	
+	private TextView mDeviceSerialNumberView;
+	private TextView mHardwareTypeView;
+	private TextView mSoftwareVersionView;
+	private TextView mLoaderVersionView;
+	private TextView mMacAddressView;
+	private TextView mGatewaySerialNumberView;
+	private TextView mServerIPView;
+	private TextView mPortView;
+	
+	private String mOriginalGatewaySerialNumber, mOriginalServerIP, mOriginalPort;
+
+	private TextView[] mEditors;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.deviceinfo_view);
+
+		initializeView();
+
+		Intent intent = getIntent();
+		mMenuPath = intent.getStringExtra(INTENT_KEY_MENUPATH);
+		showMenuPath(mMenuPath.split(MENU_STRING_DELIMITER));
+	}
+
+	public void querySettings() {
+		mService.getSettingsValue(this, GDSettings.SettingDeviceSerialNumber);
+		mService.getSettingsValue(this, GDSettings.SettingHardwareType);
+		mService.getSettingsValue(this, GDSettings.SettingSoftwareVersion);
+		mService.getSettingsValue(this, GDSettings.SettingLoaderVersion);
+		mService.getSettingsValue(this, GDSettings.SettingGatewaySerialNumber);
+		mService.getSettingsValue(this, GDSettings.SettingServerIP);
+		mService.getSettingsValue(this, GDSettings.SettingServerPort);
+		
+		String macAddress = mService.getMacAddress();
+		mMacAddressView.setText(macAddress);
+	}
+
+	public void updateSettings(String key, String value) {
+		if (key.equals(GDSettings.SettingDeviceSerialNumber)) {
+			mDeviceSerialNumberView.setText(value);
+		} else if (key.equals(GDSettings.SettingHardwareType)) {
+			mHardwareTypeView.setText(value);
+		} else if (key.equals(GDSettings.SettingSoftwareVersion)) {
+			mSoftwareVersionView.setText(value);
+		} else if (key.equals(GDSettings.SettingLoaderVersion)) {
+			mLoaderVersionView.setText(value);
+		} else if (key.equals(GDSettings.SettingGatewaySerialNumber)) {
+			mOriginalGatewaySerialNumber= value; 
+			mGatewaySerialNumberView.setText(value);
+		} else if (key.equals(GDSettings.SettingServerIP)) {
+			mOriginalServerIP = value;
+			mServerIPView.setText(value);
+		} else if (key.equals(GDSettings.SettingServerPort)) {
+			mOriginalPort = value;
+			mPortView.setText(value);
+		} else {
+		}
+	}	
+	
+	public void saveSettings() {
+		if (!mBound)
+			return;
+
+		String gatewaySerialNumber = mGatewaySerialNumberView.getText().toString();
+		String serverIP = mServerIPView.getText().toString();
+		String serverPort = mPortView.getText().toString();
+
+		/*
+		 * if (!version.isEmpty()) {
+		 * mService.setSettingsValue(GDSettings.SettingVersion, version); }
+		 */
+
+		if (!gatewaySerialNumber.isEmpty() && !gatewaySerialNumber.equals(mOriginalGatewaySerialNumber)) {
+			mService.setSettingsValue(GDSettings.SettingGatewaySerialNumber, gatewaySerialNumber);
+		}
+
+		if (!serverIP.isEmpty() && !serverIP.equals(mOriginalServerIP)) {
+			mService.setSettingsValue(GDSettings.SettingServerIP, serverIP);
+		}
+
+		if (!serverPort.isEmpty() && !serverPort.equals(mOriginalPort)) {
+			mService.setSettingsValue(GDSettings.SettingServerPort, serverPort);
+		}
+	}
+	
+
+	public void initializeView () {
+		super.initializeView();
+		
+		mDeviceSerialNumberView = (TextView) findViewById(R.id.text_device_serialnumber);
+		mHardwareTypeView = (TextView) findViewById(R.id.text_hardware_type);
+		mSoftwareVersionView = (TextView) findViewById(R.id.text_software_version);
+		mLoaderVersionView = (TextView) findViewById(R.id.text_loader_version);
+		mMacAddressView = (TextView) findViewById(R.id.text_mac_address);
+		mGatewaySerialNumberView = (TextView) findViewById(R.id.text_gateway_serialnumber);
+		mServerIPView = (TextView) findViewById(R.id.text_serverip);
+		mPortView = (TextView) findViewById(R.id.text_serverport);
+
+		mEditors = new TextView[3];
+		mEditors[0] = mGatewaySerialNumberView;
+		mEditors[1] = mServerIPView;
+		mEditors[2] = mPortView;
+
+		for (int i = 0; i < mEditors.length; i++) {
+			mEditors[i].setOnKeyListener(new View.OnKeyListener() {
+
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					Log.d(TAG, "onKey " + keyCode);
+					boolean ret = false;
+					int action = event.getAction();
+					if (action == KeyEvent.ACTION_DOWN) {
+						switch (keyCode) {
+
+						case KeyEvent.KEYCODE_DPAD_CENTER:
+						case KeyEvent.KEYCODE_ENTER:
+							ret = true;
+							showInputView(v);
+							break;
+						default:
+							break;
+						}
+					}
+					return ret;
+				}
+			});
+		}
+	}
+}
--- DbstarLauncher/src/com/dbstar/app/GDUserInfoActivity.java
+++ DbstarLauncher/src/com/dbstar/app/GDUserInfoActivity.java
@@ -4,25 +4,19 @@ import com.dbstar.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class GDUserInfoActivity extends GDBaseActivity {
+public class GDUserInfoActivity extends GDSettingActivity {
 
-	private TextView mVersionView, mCardNumberView, mMacAddressView,
-			mCardValidityTermView, mUerBussiness, mUserResidualAmountView;
+	private TextView mOperatorView, mCardNumberView;
+	ListView mProductView;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.userinfo_view);
 
-		mVersionView = (TextView) findViewById(R.id.text_version);
-		mCardNumberView = (TextView) findViewById(R.id.text_usercardnumber);
-		mMacAddressView = (TextView) findViewById(R.id.text_macaddress);
-		mCardValidityTermView = (TextView) findViewById(R.id.text_usercard_validityterm);
-		mUerBussiness = (TextView) findViewById(R.id.text_user_bussiness);
-		mUserResidualAmountView = (TextView) findViewById(R.id.text_user_residual_amount);
-		
 		initializeView();
 		Intent intent = getIntent();
 		mMenuPath = intent.getStringExtra(INTENT_KEY_MENUPATH);
@@ -31,10 +25,14 @@ public class GDUserInfoActivity extends GDBaseActivity {
 	
 	public void onServiceStart() {
 		super.onServiceStart();
+	}
+	
+	public void initializeView() {
+		super.initializeView();
 		
-		//TODO: the network maybe not startup, so this call may not get the mac address.
-		//TODO: so consider using some other mechanism.
-		String macAddr = mService.getMacAddress();
-		mMacAddressView.setText(macAddr);
+		mOperatorView = (TextView) findViewById(R.id.text_operator);
+		mCardNumberView = (TextView) findViewById(R.id.text_usercardnumber);
+		
+		mProductView = (ListView) findViewById(R.id.userinfo_orderedproduct);
 	}
 }
