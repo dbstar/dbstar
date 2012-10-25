bb5ec959e1cb094cca3ba5b269aa6f8cca7d6861
Luo Shidong <luosd09@126.com>
2012-10-25 22:56:10 +0800

    Launcher: add missing files

---
+++ DbstarLauncher/src/com/dbstar/model/EventData.java
@@ -0,0 +1,10 @@
+package com.dbstar.model;
+
+public class EventData {
+	public static final int EVENT_DELETE = 0;
+	
+	public static class DeleteEvent {
+		public String PublicationId;
+		public String PublicationSetId;
+	} 
+}
