2bde7c2e07b96e26adef559e2d461d47e7c6ee33
Luo Shidong <luosd09@126.com>
2012-11-11 20:40:08 +0800

    Settings: fix errors

--- DbstarSettings/Android.mk
+++ DbstarSettings/Android.mk
@@ -1,18 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+#LOCAL_JAVA_LIBRARIES := bouncycastle
+#LOCAL_STATIC_JAVA_LIBRARIES := guava
+
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files) \
-	src/com/dbstar/DbstarDVB/IDbstarService.aidl \
-#	./src/com/dbstar/DbstarDVB/PlayerService/IPlayerService.aidl
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := DbstarDVB
+LOCAL_PACKAGE_NAME := DbstarSettings
 LOCAL_CERTIFICATE := platform
-LOCAL_STATIC_JAVA_LIBRARIES := amlogic.subtitle
-LOCAL_REQUIRED_MODULES := libamplayerjni libsubjni
 
-LOCAL_PROGUARD_ENABLED := disabled
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
