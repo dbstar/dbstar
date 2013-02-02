41784d27c4954de2093eff5a7ae9c0b595749ff3
peifu.jiang <jiangpeifu@gmail.com>
2013-02-02 20:51:44 +0800

    rootfs: update init.

--- rootfs/Android.mk
+++ rootfs/Android.mk
@@ -1,3 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 $(shell cd $(LOCAL_PATH) && mkdir -p $(ANDROID_PRODUCT_OUT)/system && cp -rf system/* $(ANDROID_PRODUCT_OUT)/system)
+$(shell cd $(LOCAL_PATH) && mkdir -p $(ANDROID_PRODUCT_OUT)/root && cp -rf root/* $(ANDROID_PRODUCT_OUT)/root)
