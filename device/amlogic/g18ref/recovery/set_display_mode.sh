aee1922ed590ca9f7a84a0cc13fcc81e0460f236
peifu.jiang <jiangpeifu@126.com>
2013-06-23 02:30:32 +0800

    device: porting m6 board.

--- device/amlogic/g18ref/recovery/set_display_mode.sh
+++ device/amlogic/g18ref/recovery/set_display_mode.sh
@@ -1,41 +1,72 @@
 #!/system/bin/sh
 
-case `getprop sys.fb.bits` in
-    32) osd_bits=32
-    ;;
 
-    *) osd_bits=16
-    ;;
-esac
+#echo "grep 720x480"
+busybox grep "w:720,h:480" /sys/class/ppmgr/ppscaler_rect
+format480=$?
 
-case $1 in 
-    480p)
-        fbset -fb /dev/graphics/fb0 -g 720 480 720 960 $osd_bits
-        echo $1 > /sys/class/display/mode
-        echo 0 0 720 480 0 0 18 18 > /sys/class/display/axis
-    ;;
+#echo "grep 720x576"
+busybox grep "w:720,h:576" /sys/class/ppmgr/ppscaler_rect
+format576=$?
 
-    720p)
-        fbset -fb /dev/graphics/fb0 -g 1280 720 1280 1440 $osd_bits
-        echo $1 > /sys/class/display/mode
-        echo 0 0 1280 720 0 0 18 18 > /sys/class/display/axis
-    ;;
+#echo "grep 1280x720"
+busybox grep "w:1280,h:720" /sys/class/ppmgr/ppscaler_rect
+format720=$?
 
-    1080p)
-        fbset -fb /dev/graphics/fb0 -g 1920 1080 1920 2160 $osd_bits
-        echo $1 > /sys/class/display/mode
-        echo 0 0 1920 1080 0 0 18 18 > /sys/class/display/axis
+#echo "grep 1920x1080"
+busybox grep "w:1920,h:1080" /sys/class/ppmgr/ppscaler_rect
+format1080=$?
+
+#echo "format480 is: "$format480
+#echo "format576 is: "$format576
+#echo "format720 is: "$format720
+#echo "format1080 is: "$format1080
+
+if [ $format480 -eq 0 ]
+then
+        echo "format480 match!"
+        format=480
+elif [ $format576 -eq 0 ]
+then
+        echo "format576 match!"
+        format=576
+elif [ $format720 -eq 0 ]
+then
+        echo "format720 match!"
+        format=720
+elif [ $format1080 -eq 0 ]
+then
+        echo "format1080 match!"
+        format=1080
+else
+        echo "no format match, set to default?"
+fi
+busybox fbset -fb /dev/graphics/fb0 -g 1280 720 1280 1440 16
+busybox fbset -fb /dev/graphics/fb0 -g 1280 720 1280 1440 16
+
+case $format in 
+
+    720)
+       
+       busybox echo 40 25 1240 690 0 > /sys/class/ppmgr/ppscaler_rect
+    ;;
+    
+    1080)
+    
+      busybox  echo 60 40 1900 1060 0 > /sys/class/ppmgr/ppscaler_rect
     ;;
 
     *)
-        echo "Error: Un-supported display mode $1"
-        echo "       Default to 720p"
-        fbset -fb /dev/graphics/fb0 -g 1280 720 1280 1440 $osd_bits
-        echo $1 > /sys/class/display/mode
-        echo 0 0 1280 720 0 0 18 18 > /sys/class/display/axis
+     
+       busybox echo 30 20 680 460 0 > /sys/class/ppmgr/ppscaler_rect
+        
 esac
 
+#busybox echo 1 > /sys/class/graphics/fb0/free_scale
+#busybox echo 1 > /sys/class/ppmgr/ppscaler
+#echo 1>/sys/class/graphics/fb1/blank
+
 busybox echo null > /sys/class/display2/mode
-busybox echo 576cvbs > /sys/class/display2/mode
+busybox echo 576cvbs > /sys/class/display2/mode 
 busybox echo 1 > /sys/class/video2/screen_mode
-busybox echo 0 > /sys/module/amvideo2/parameters/clone_frame_scale_width
+busybox echo 0  > /sys/module/amvideo2/parameters/clone_frame_scale_width
